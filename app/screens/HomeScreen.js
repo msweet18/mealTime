109a59c025c92f8128b48f69930776bd4fcd79c8
Maximillian Sweet <msweet@globalmed.com>
2019-10-31 11:37:58 -0700

    add state management

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+/**
+ * The application Home Screen.
+ * @param {Object} props - The properties available to the component
+ */
+const HomeScreen = props => {
+  return (
+    <View>
+      <Text>
+        Welcome to Meal Time.
+      </Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
